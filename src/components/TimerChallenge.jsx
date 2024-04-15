335cd4711df4940f84bd59864e84d1f97a4c99e0
rebs-code <jack.rebussi@gmail.com>
2024-04-15 12:33:35 +0200

    Refactor ResultModal component to display remaining time in seconds

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -9,6 +9,10 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
     setTimeRemaining(targetTime * 1000);
   }
 
@@ -19,12 +23,18 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   function handleStop() {
+    dialog.current.open();
     clearIntervak(timer.current);
   }
 
   return (
     <>
-      <ResultModal targetTime={targetTime} result="lost" ref={dialog} />
+      <ResultModal
+        targetTime={targetTime}
+        ref={dialog}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
 
       <section className="challenge">
         <h2>{title}</h2>
